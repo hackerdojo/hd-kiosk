434c4ca441638b8c82c7c4764c74bf34156c9dfc
Brian Klug <brian.klug@gmail.com>
2011-03-02 11:29:08 -0800

    Fix refresh on kiosk

--- static/main.js
+++ static/main.js
@@ -14,15 +14,23 @@ function refreshPage() {
   $.ajax({
     url: '/',
     success: function(data) {
+      var tmp = window.i;
       if (window.oldint) {
         clearTimeout(window.oldint);
       }      
       $('#body').html(data);
+      if (window.oldint) {
+        clearTimeout(window.oldint);
+      }      
+      window.i = tmp + 1;
+      $('#reloadcount').html(" "+window.i);
+      window.oldint = setInterval(refreshPage, 2 * 60 * 1000);
     }
   });
 }
 
 $(document).ready(function(){
-    window.oldint = setInterval(refreshPage,3 * 60 * 1000);
+    window.i = 0;
+    window.oldint = setInterval(refreshPage,2 * 60 * 1000);
     letsDance();
 });
